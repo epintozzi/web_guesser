5b982f26fef1a55fa3d2082a6f44328b524eff89
Erin Pintozzi <erin.pintozzi@gmail.com>
2016-09-26 12:00:07 -0700

    I4: added colored background to correlate with closeness of guess

--- web_guesser.rb
+++ web_guesser.rb
@@ -6,7 +6,8 @@ SECRET_NUMBER = rand(100)
 get '/' do
   guess = params["guess"].to_i
   message = check_guess(guess)
-  erb :index, :locals => {:number => SECRET_NUMBER, :message => message}
+  color = get_color(guess)
+  erb :index, :locals => {:message => message, :color => color, :guess => guess}
 end
 
 def check_guess(guess)
@@ -19,16 +20,16 @@ def check_guess(guess)
   elsif guess > SECRET_NUMBER
     message = "Too high!"
   else
-    message = "You got it right! The number is #{SECRET_NUMBER}"
+    message = "You got it right! The number is #{SECRET_NUMBER}."
   end
 end
 
-# def check_guess(guess)
-  # if guess < number
-  #   message = "Too high!"
-  # elsif guess > number
-  #   message = "Too low!"
-  # elsif guess == number
-  #   message = "You got it right!"
-  # end
-# end
+def get_color(guess)
+  if guess > (SECRET_NUMBER + 5) || guess < (SECRET_NUMBER - 5)
+    color = "red"
+  elsif guess < SECRET_NUMBER || guess > SECRET_NUMBER
+    color = "lightsalmon"
+  else
+    color = "green"
+  end
+end
