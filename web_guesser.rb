fb7711c148e353401647968b20278013c8d0d10e
Erin Pintozzi <erin.pintozzi@gmail.com>
2016-09-26 10:45:47 -0700

    I1: generates a random number

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,5 +1,15 @@
 require 'sinatra'
+require 'sinatra/reloader'
+
+# get '/' do
+#   rng = Random.new(8)
+#   a = (0..100).to_a
+#   number = a.sample(random: rng)
+#   "The SECRET NUMBER is #{number}."
+# end
+
+number = rand(100)
 
 get '/' do
-  "Hello, World!"
+  "The SECRET NUMBER is #{number}."
 end
